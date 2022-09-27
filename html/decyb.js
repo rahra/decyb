925965b71225fa1c3fcdc0ddb24fa6fda9cff63e
Bernhard R. Fischer <bf@abenteuerland.at>
2022-09-27 21:57:14 +0200

    further improvements

--- html/decyb.js
+++ html/decyb.js
@@ -1,5 +1,10 @@
-const NDIST = 18;
+const NDIST = 20;
 const TEXTX = 80;
+const BORDER = 0.025;
+
+const cscheme_ = [{bg: "#171717", cap: "#b0b0b0", xbg: "#b0b0b030", tx: "#c0c0c0"}, {bg: "#e8e8d8", cap: "#404040", xbg: "#000000b0", tx: "#e8e8e8"}];
+var cur_scheme_ = 0;
+var col_ = cscheme_[cur_scheme_];
 
 
 function parse(e)
@@ -106,29 +111,53 @@ function calc_poi(moments)
             ix = i;
          }
       }
-      moments[ix].name = nodes_[j].name;
+
+      // Mark chosen trackpoint if distance < 100 nm. (It's assumed the the point was not reached of the distance is >100 nm.)
+      if (dist < 100)
+         moments[ix].name = nodes_[j].name;
    }
 }
 
 
-function calc_moments(moments)
+function calc_moments(moments, t_min)
 {
+   var ix = -1, v_avg = 0;
+
+   // set distance of 1st point to 0
    moments[moments.length - 1].td = moments[moments.length - 1].dist = moments[moments.length - 1].dist_tot = 0;
    for (var i = moments.length - 1; i; i--)
    {
+      // ignore data before start of race
+      if (moments[i - 1].at <= t_min)
+      {
+         moments[i - 1].td = moments[i - 1].dist = moments[i - 1].dist_tot = 0;
+         continue;
+      }
       coord_diff(moments[i], moments[i - 1]);
       moments[i - 1].dist_tot = moments[i - 1].dist + moments[i].dist_tot;
       moments[i - 1].td = moments[i - 1].at - moments[i].at;
-      moments[i - 1].v_avg = moments[i - 1].dist / moments[i - 1].td * 3600;
+      moments[i - 1].v_avg = moments[i - 1].td ? moments[i - 1].dist / moments[i - 1].td * 3600 : 0;
+
+      // find max avg speed
+      if (moments[i - 1].v_avg > v_avg)
+      {
+         v_avg = moments[i - 1].v_avg;
+         ix = i - 1;
+      }
    }
+
+   // set max speed
+   if (ix != -1)
+      moments[ix].v_avg_max = 1;
 }
  
 
-function calc_data(data_)
+function calc_data(setup_, data_)
 {
    for (var i = 0; i < data_.length; i++)
    {
-      calc_moments(data_[i].moments);
+      setup_.teams[i].visible = 0;
+      calc_moments(data_[i].moments, setup_.teams[i].start);
       calc_poi(data_[i].moments);
    }
 }
@@ -142,15 +171,31 @@ function data_check(setup_, data_)
 }
 
 
-function draw_v_avg(ctx, C, moments)
+function draw_v_avg(ctx, C, moments, setup)
 {
+   var ix = -1;
+
    ctx.save();
    ctx.translate(0, C.d_max * C.sy);
    ctx.beginPath();
-   ctx.moveTo(0, 0);
+   ctx.moveTo(0, C.v_max * C.sy2);
    for (var i = moments.length - 1; i >= 0; i--)
+   {
       ctx.lineTo((moments[i].at - C.t_min) * C.sx, (C.v_max - moments[i].v_avg) * C.sy2);
+      if (moments[i].hasOwnProperty("v_avg_max"))
+         ix = i;
+   }
    ctx.stroke();
+
+   if (ix >= 0)
+   {
+      //ctx.strokeStyle = "#f00000";
+      ctx.fillStyle = "#f00000";
+      ctx.beginPath();
+      ctx.arc((moments[ix].at - C.t_min) * C.sx, (C.v_max - moments[ix].v_avg) * C.sy2, 3, 0, 2 * Math.PI);
+      ctx.fill();
+      ctx.fillText("v_avg_max = " + moments[ix].v_avg.toFixed(1), (moments[ix].at - C.t_min) * C.sx, (C.v_max - moments[ix].v_avg) * C.sy2 - 3);
+   }
    ctx.restore();
 }
 
@@ -160,7 +205,13 @@ function draw_moments(ctx, C, moments)
    ctx.beginPath();
    ctx.moveTo(0, C.d_max * C.sy);
    for (var i = moments.length - 1; i >= 0; i--)
+   {
+      // ignore everything before race start
+      if (moments[i].at < C.t_min)
+         continue;
+
       ctx.lineTo((moments[i].at - C.t_min) * C.sx, (C.d_max - moments[i].dist_tot) * C.sy);
+   }
    ctx.stroke();
 }
 
@@ -185,12 +236,22 @@ function draw_marks(ctx, C, moments)
 function caption(ctx, C)
 {
    var text = title_.split("\n");
+   var w = 0;
+
+   for (var i = 0; i < text.length; i++)
+      w = Math.max(w, ctx.measureText(text[i]).width);
 
    ctx.save();
-   ctx.fillStyle = "#b0b0b0";
+   ctx.beginPath();
+   ctx.fillStyle = col_.xbg;
+   ctx.rect((C.t_max - C.t_min) * C.sx * 0.33 - 5, 0, w + 10, 20 * (text.length + .5));
+   ctx.fill();
+
+   ctx.beginPath();
+   ctx.fillStyle = col_.tx;
    for (var i = 0; i < text.length; i++)
       ctx.fillText(text[i], (C.t_max - C.t_min) * C.sx * 0.33, 20 * (i + 1));
-   ctx.restore();
+  ctx.restore();
 }
 
 
@@ -200,8 +261,8 @@ function axis(ctx, C)
    const YDIFF = 20;
 
    ctx.save();
-   ctx.fillStyle = "#b0b0b0";
-   ctx.strokeStyle = "#b0b0b0";
+   ctx.fillStyle = col_.cap;
+   ctx.strokeStyle = col_.cap;
    ctx.lineWidth = 1;
    ctx.setLineDash([6,4]);
 
@@ -261,12 +322,14 @@ function axis(ctx, C)
 function draw_data(setup_, data_)
 {
    //data_check(setup_, data_);
-   document.body.style.backgroundColor = "#171717";
+   document.body.style.backgroundColor = col_.bg;
    var canvas = document.getElementById("chart");
    var ctx = canvas.getContext("2d");
-   canvas.width = window.innerWidth;
-   canvas.height = window.innerHeight;
-   //ctx.translate(canvas.width * 0.025, -canvas.height * 0.025);
+   var width = canvas.width = window.innerWidth;
+   var height = canvas.height = window.innerHeight;
+
+   ctx.translate(width * BORDER, height * BORDER);
+   //ctx.scale(0.95, 0.95);
 
    // drawing parameters
    var C =
@@ -280,18 +343,18 @@ function draw_data(setup_, data_)
          sy2: 0,
       };
 
+   C.t_min = setup_.teams[0].start;
    for (var i = 0; i < data_.length; i++)
    {
-      C.t_min = Math.min(C.t_min, data_[i].moments[data_[i].moments.length - 1].at);
+      //C.t_min = Math.min(C.t_min, data_[i].moments[data_[i].moments.length - 1].at);
       C.t_max = Math.max(C.t_max, data_[i].moments[0].at);
       C.d_max = Math.max(C.d_max, data_[i].moments[0].dist_tot);
    }
 
    var ysplit = 0.8;
-   C.sx = canvas.width / (C.t_max - C.t_min);
-   C.sy = canvas.height / C.d_max * ysplit;
-   C.sy2 = canvas.height / C.v_max * (1 - ysplit);
-   //console.log(C);
+   C.sx = width / (C.t_max - C.t_min) * (1 - 2 * BORDER);
+   C.sy = height / C.d_max * ysplit * (1 - 2 * BORDER);
+   C.sy2 = height / C.v_max * (1 - ysplit) * (1 - 2 * BORDER);
 
    ctx.lineWidth = 1;
    ctx.font = "14px sans-serif";
@@ -299,10 +362,14 @@ function draw_data(setup_, data_)
    axis(ctx, C);
    caption(ctx, C);
 
+   ctx.fillStyle = col_.xbg;
+   ctx.rect(TEXTX - 10, 0, 370, data_.length * NDIST + 10);
+   ctx.fill();
+
    for (var i = 0; i < data_.length; i++)
    {
       //if (data_[i].id != 3) continue;
-      if (_a == i)
+      if (_a == i || setup_.teams[i].visible)
       {
          o = "ff";
          ctx.lineWidth = 3;
@@ -310,29 +377,56 @@ function draw_data(setup_, data_)
       }
       else
       {
-         o = "c0";
+         o = "e0";
          ctx.lineWidth = 1;
          ctx.font = "14px sans-serif";
       }
       ctx.strokeStyle = "#" + setup_.teams[i].colour + o;
       ctx.fillStyle = "#" + setup_.teams[i].colour + o;
+
+      var v_avg = data_[i].moments[0].dist_tot * 3600 / (data_[i].moments[0].at - data_[i].moments[data_[i].moments.length - 1].at);
+      ctx.fillText(setup_.teams[i].name + ", dist = " + data_[i].moments[0].dist_tot.toFixed(1) + ", v_avg = " + v_avg.toFixed(2), TEXTX, (i+1) * NDIST);
+
+      if (!setup_.teams[i].visible && _a != i)
+         continue;
+
       draw_moments(ctx, C, data_[i].moments);
       draw_marks(ctx, C, data_[i].moments);
       draw_v_avg(ctx, C, data_[i].moments);
-      var v_avg = data_[i].moments[0].dist_tot * 3600 / (data_[i].moments[0].at - data_[i].moments[data_[i].moments.length - 1].at);
-      ctx.fillText("(" + i + ") " + setup_.teams[i].name + ", dist = " + data_[i].moments[0].dist_tot.toFixed(1) + ", v_avg = " + v_avg.toFixed(2), TEXTX, (i+1) * NDIST);
    }
 }
 
 
 var _s, _j, _a = -1;
 
-function mouse_move_handler(e)
+function handle_mouse_pos(e)
 {
    var mx = e.pageX - document.getElementById("chart").getBoundingClientRect().left;
    var my = e.pageY - document.getElementById("chart").getBoundingClientRect().top;
 
-   _a = mx >= TEXTX && mx < TEXTX + 350 && my < _j.length * NDIST ? Math.floor(my / NDIST) : -1;
+   var x = window.innerWidth * BORDER;
+   var y = window.innerHeight * BORDER;
+   _a = mx >= x + TEXTX && mx < x + TEXTX + 350 && my >= y && my < _j.length * NDIST + y ? Math.floor((my - y) / NDIST) : -1;
+}
+
+function mouse_move_handler(e)
+{
+   handle_mouse_pos(e);
+   update_graph();
+}
+
+function mouse_click_handler(e)
+{
+   handle_mouse_pos(e);
+
+   if (_a >= 0 && _a < _s.teams.length)
+      _s.teams[_a].visible ^= 1;
+   else
+   {
+      cur_scheme_ = (cur_scheme_ + 1) % cscheme_.length;
+      col_ = cscheme_[cur_scheme_];
+   }
+
    update_graph();
 }
 
@@ -363,7 +457,7 @@ function get_data(server)
          //console.log(setup);
 
          save_data(setup, jdata);
-         calc_data(jdata);
+         calc_data(setup, jdata);
          //document.getElementById("pre").innerHTML = JSON.stringify(jdata, null, 2);
          //draw_data(setup, jdata);
          update_graph();
