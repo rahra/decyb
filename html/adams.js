8459a1927911a03ec3cdf688e4014a3c4245bc8e
Bernhard R. Fischer <bf@abenteuerland.at>
2026-09-15 23:08:02 +0200

    added inverse Adams II

--- html/adams.js
+++ html/adams.js
@@ -7,9 +7,10 @@
  *
  * \file adams.js
  * \author Bernhard R. Fischer <bf@abenteuerland.at>
- * \date 2022/10/04
+ * \date 2026/09/14
  */
 
+const M_PI = Math.PI;
 const M_PI_2 = Math.PI / 2;
 const C1 = 1e-3;
 const C2  = 1e-9;
@@ -132,5 +133,89 @@ function adams_square_ii(lambda, phi)
    xy = elliptic_factory(a, b, sm, sn);
 
    return {x: Math.SQRT1_2 * (xy.x - xy.y), y: Math.SQRT1_2 * (xy.x + xy.y)};
+}
+
+
+function limit(a, b)
+{
+   if (a < -b)
+      return -b;
+
+   if (a > b)
+      return b;
+
+   return a;
+}
+
+function inverse(x, y, lam, phi, proj)
+{
+   var lam2, phi2, dlam0, dphi0, det;
+   var dlam = {x: 0, y: 0}, dphi = {x: 0, y: 0}, appr = {x: 0, y: 0}, d = {x: 0, y: 0}, xy2 = {x: 0, y: 0}, dtlam = {x: 0, y: 0}, dtphi = {x: 0, y: 0};
+
+   dlam.x = dlam.y = 0;
+   dphi.x = dphi.y = 0;
+
+   for (var i = 0; i < 15; i++)
+   {
+      appr = proj(lam, phi);
+      d.x = appr.x - x;
+      d.y = appr.y - y;
+
+      if (Math.abs(d.x) < 1e-10 && Math.abs(d.y) < 1e-10)
+         return {x: lam, y: phi};
+
+      if (Math.abs(d.x) > 1e-6 || Math.abs(d.y) > 1e-6)
+      {
+         dlam0 = lam > 0 ? -1e-6 : 1e-6;
+         lam2 = lam + dlam0;
+         phi2 = phi;
+         xy2 = proj(lam2, phi2);
+         dtlam.x = (xy2.x - appr.x) / dlam0;
+         dtlam.y = (xy2.y - appr.y) / dlam0;
+
+         dphi0 = phi > 0 ? -1e-6 : 1e-6;
+         lam2 = lam;
+         phi2 = phi + dphi0;
+         xy2 = proj(lam2, phi2);
+         dtphi.x = (xy2.x - appr.x) / dphi0;
+         dtphi.y = (xy2.y - appr.y) / dphi0;
+
+         det = dtlam.x * dtphi.y - dtphi.x * dtlam.y;
+         if (det != 0)
+         {
+            dlam.x =  dtphi.y / det;
+            dlam.y = -dtphi.x / det;
+            dphi.x = -dtlam.y / det;
+            dphi.y =  dtlam.x / det;
+         }
+      }
+
+      if (x != 0)
+      {
+         dlam0 = Math.max(Math.min(d.x * dlam.x + d.y * dlam.y, 0.3), -0.3);
+         lam -= dlam0;
+         lam = limit(lam, M_PI);
+      }
+
+      if (y != 0)
+      {
+         dphi0 = Math.max(Math.min(d.x * dphi.x + d.y * dphi.y, 0.3), -0.3);
+         phi -= dphi0;
+         phi = limit(phi, M_PI_2);
+      }
+   }
+
+   return {x: lam, y: phi};
+}
+
+
+function adams_square_ii_invert(x, y)
+{
+   var phi, lam;
+
+   phi = Math.max(Math.min(y / A2_PHI_SCALE, 1), -1) * M_PI_2;
+   lam = Math.abs(phi) < M_PI ? Math.max(Math.min(x / A2_LAM_SCALE / Math.cos(phi), 1), -1) * M_PI : 0;
+
+   return inverse(x, y, lam, phi, adams_square_ii);
 }
 
